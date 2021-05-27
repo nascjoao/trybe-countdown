aee18f1ce8bb92ae9e3478db1587452c35ccd7c0
nascjoao <oi@joaonasc.dev>
2021-05-27 15:09:04 -0300

    feat: create routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import Countdown from './components/Countdown';
 import Menu from './components/Menu';
+import About from './components/About';
+import Preferences from './components/Preferences';
 
 function App() {
   return (
     <>
       <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/settings" component={Preferences} />
         <Route path="/">
           <Menu/>
           <Countdown />
